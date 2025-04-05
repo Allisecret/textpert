f5a653e379d46338295bb7cf47dc8327d986e0e5
Roeich <roeichsheikh@gmail.com>
2025-04-05 21:29:27 +0600

    completed all pages

---
+++ assets/js/dashboard.js
@@ -0,0 +1,6 @@
+document.getElementById("show_sidemenu").addEventListener("click",function(){
+    document.getElementById("sidemeu").classList.add("show_sidebar");
+});
+document.getElementById("hide_sidemenu").addEventListener("click",function(){
+    document.getElementById("sidemeu").classList.remove("show_sidebar");
+});
